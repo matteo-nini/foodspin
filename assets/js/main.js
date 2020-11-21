c2cc6e6b46f9bd0dedf0f57c3b99a10c324cf577
Matteo <matteonini95@gmail.com>
2020-11-21 09:51:51 +0100

    [DEPLOY] Info section and  change colors functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,54 +1,106 @@
+//Take elements
 const container = document.querySelector(".spinner__container");
-const dishes = document.querySelectorAll(".spinner__dish");
 const wheel = document.querySelector(".spinner__wheel");
-const activeDish = document.querySelector(".dish");
+const dishes = document.querySelectorAll(".spinner__dish");
 
-let i = 0;
-const colors = ["#ffeedc", "#EAFFE2"];
+const selectedDish = document.querySelector(".selected__dish");
 
+const shiftLeft = document.querySelector(".shift__left");
+const shiftRight = document.querySelector(".shift__right");
+
+const price = document.querySelector("h2");
+const name = document.querySelector("h3");
+const description = document.querySelector("p");
+const orderNow = document.querySelector(".selected__info button");
+
+//Define arrays
+const images = [
+  "url('assets/images/webp/dish_1.webp')",
+  "url('assets/images/webp/dish_2.webp')",
+  "url('assets/images/webp/dish_3.webp')",
+];
+
+const prices = [];
+const names = [];
+const descriptions = [];
+
+//arrays index
+let imageSelector = 0; //image selector
+
+//boolean for colors control
+let isOrange = true;
+
+//define the center for the elements on the spinner
 const center = {
   x: parseFloat(getComputedStyle(dishes[0]).left), //take the distance from the left
   y: parseFloat(getComputedStyle(dishes[0]).top), ////take the distance from the top
 };
 
-let angle = Math.PI / 4.0; //45 deg
+// define the variables needed for positioning
 let newAngle = 0.0;
 let newX = 0.0;
 let newY = 0.0;
-let spinRadius = 280.0; //radius distance from the center
-let shift = 0.0;
 
+//define variables for spinning
+let spin = 0.0;
+
+//positioning the elements on the spinner
 dishes.forEach((dish, i) => {
+  const angle = Math.PI / 4.0; //angle of 45 deg on the wheel
+  const spinRadius = 280.0; //distance from the center
+
   newAngle = angle * i;
   newX = Math.cos(newAngle) * spinRadius;
   newY = -1.0 * Math.sin(newAngle) * spinRadius;
 
   dish.style.left = `${center.x + newX}px`;
   dish.style.top = `${center.y + newY}px`;
+
+  //add the elements relative bg
+  imageSelector++;
+  if (imageSelector > images.length - 1) imageSelector = 0;
+  dish.style.backgroundImage = images[imageSelector];
 });
 
-let shiftLeft = document.querySelector(".shift__left");
-let shiftRight = document.querySelector(".shift__right");
-
+//button right On click
 shiftRight.addEventListener("click", (event) => {
-  let initShift = 360 / 8;
-  shift += initShift;
-  wheel.style.transform = `translate(-50%, -50%) rotate(${shift}deg)`;
-  changeBgColor();
+  changeColors();
+  rightSliding();
+  changeImage();
 });
 
+//button left on click
 shiftLeft.addEventListener("click", (event) => {
-  let initShift = 360 / 8;
-  shift -= initShift;
-  wheel.style.transform = `translate(-50%, -50%) rotate(${shift}deg)`;
-  changeBgColor();
+  leftSliding();
+  changeColors();
 });
 
-function changeBgColor() {
-  if (container.style.background == "#EAFFE2")
-    container.style.background = "#ffeede";
-  else container.style.background = "#EAFFE2";
-  if (container.style.background == "#ffeede")
-    container.style.background = "#EAFFE2";
-  else container.style.background = "#ffeede";
+function rightSliding() {
+  const slice = 360 / 8;
+  spin += slice;
+  wheel.style.transform = `translate(-50%, -50%) rotate(${spin}deg)`;
+}
+
+function leftSliding() {
+  const slice = 360 / 8;
+  spin -= slice;
+  wheel.style.transform = `translate(-50%, -50%) rotate(${spin}deg)`;
+}
+
+function changeColors() {
+  if (isOrange == true) {
+    container.classList.add("bg--green");
+    shiftRight.classList.add("btn--green");
+    shiftLeft.classList.add("btn--green");
+    price.style.color = "#54bf29";
+    orderNow.classList.add("btn--green");
+    isOrange = false;
+  } else {
+    container.classList.remove("bg--green");
+    shiftRight.classList.remove("btn--green");
+    shiftLeft.classList.remove("btn--green");
+    price.style.color = "#ff922c";
+    orderNow.classList.remove("btn--green");
+    isOrange = true;
+  }
 }
