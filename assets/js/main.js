e87ae2d75c04abba8172b7223ae2951d93b7462a
Matteo <matteonini95@gmail.com>
2020-11-21 14:26:54 +0100

    [DEPLOY] Dynamic content & Animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-//Take elements
+//Take needed elements
 const container = document.querySelector(".spinner__container");
 const wheel = document.querySelector(".spinner__wheel");
 const dishes = document.querySelectorAll(".spinner__dish");
@@ -19,13 +19,21 @@ const images = [
   "url('assets/images/webp/dish_2.webp')",
   "url('assets/images/webp/dish_3.webp')",
 ];
-
-const prices = [];
-const names = [];
-const descriptions = [];
+const prices = ["$32", "$35", "$32"];
+const names = [
+  "Green Goddess Chicken Salad",
+  "Asian Cucumber Salad",
+  "Green Goddess Chicken Salad",
+];
+const descriptions = [
+  "It is a non vegetarian salad which consists of the green goddess dressing mixed with chicken, peppers, olives and celery.",
+  "Asian Cucumber Salad Recipe made with crunchy cucumber, onion, rice wine vinegar, and a few secret ingredients!",
+  "It is a non vegetarian salad which consists of the green goddess dressing mixed with chicken, peppers, olives and celery. ",
+];
 
 //arrays index
 let imageSelector = 0; //image selector
+let contentSelector = 0; //content selector
 
 //boolean for colors control
 let isOrange = true;
@@ -43,6 +51,7 @@ let newY = 0.0;
 
 //define variables for spinning
 let spin = 0.0;
+const slice = 360 / 8;
 
 //positioning the elements on the spinner
 dishes.forEach((dish, i) => {
@@ -62,30 +71,33 @@ dishes.forEach((dish, i) => {
   dish.style.backgroundImage = images[imageSelector];
 });
 
+//init the contents
+price.innerHTML = prices[contentSelector];
+name.innerHTML = names[contentSelector];
+description.innerHTML = descriptions[contentSelector];
+selectedDish.style.backgroundImage = images[contentSelector];
+
 //button right On click
 shiftRight.addEventListener("click", (event) => {
+  spin += slice;
+  wheel.style.transform = `translate(-50%, -50%) rotate(${spin}deg)`;
+  if (contentSelector >= images.length - 1) contentSelector = 0;
+  else contentSelector++;
+  animateContent();
+  changeContent(contentSelector);
   changeColors();
-  rightSliding();
-  changeImage();
 });
 
 //button left on click
 shiftLeft.addEventListener("click", (event) => {
-  leftSliding();
-  changeColors();
-});
-
-function rightSliding() {
-  const slice = 360 / 8;
-  spin += slice;
-  wheel.style.transform = `translate(-50%, -50%) rotate(${spin}deg)`;
-}
-
-function leftSliding() {
-  const slice = 360 / 8;
   spin -= slice;
   wheel.style.transform = `translate(-50%, -50%) rotate(${spin}deg)`;
-}
+  if (contentSelector <= 0) contentSelector = images.length - 1;
+  else contentSelector--;
+  animateContent();
+  changeContent(contentSelector);
+  changeColors();
+});
 
 function changeColors() {
   if (isOrange == true) {
@@ -103,4 +115,54 @@ function changeColors() {
     orderNow.classList.remove("btn--green");
     isOrange = true;
   }
+}
+
+function changeContent(contentSelector) {
+  switch (contentSelector) {
+    case 0:
+      price.innerHTML = prices[0];
+      name.innerHTML = names[0];
+      description.innerHTML = descriptions[0];
+      selectedDish.style.backgroundImage = images[0];
+      break;
+    case 1:
+      price.innerHTML = prices[1];
+      name.innerHTML = names[1];
+      description.innerHTML = descriptions[1];
+      selectedDish.style.backgroundImage = images[1];
+      break;
+    case 2:
+      price.innerHTML = prices[2];
+      name.innerHTML = names[2];
+      description.innerHTML = descriptions[2];
+      selectedDish.style.backgroundImage = images[2];
+      break;
+  }
+}
+
+function animateContent() {
+  //Animation keyframes
+  let dishAnimate = [
+    { transform: "rotate(-45deg)" },
+    { transform: "scale(0.5)" },
+    { transform: "scale(1)" },
+  ];
+
+  let textAnimate = [
+    { transform: "scale(1, 1)" },
+    { transform: "scale(0.8, 0.8)" },
+    { transform: "scale(0.5, 0.5)" },
+    { transform: "scale(0.2, 0.5)" },
+    { transform: "scale(0, 0)" },
+    { transform: "scale(0.2, 0.5)" },
+    { transform: "scale(0.5, 0.5)" },
+    { transform: "scale(0.8, 0.8)" },
+    { transform: "scale(1, 1)" },
+  ];
+
+  selectedDish.animate(dishAnimate, 300);
+  price.animate(textAnimate, 500);
+  name.animate(textAnimate, 500);
+  description.animate(textAnimate, 500);
+  orderNow.animate(textAnimate, 500);
 }
